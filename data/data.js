3f229c3f021a79de96c9aad19fb9db987146d1f3
smriti <rajsimmi369@gmail.com>
2024-12-21 00:27:08 +0530

    Added Styled movie cards with hover effects

---
+++ data/data.js
@@ -0,0 +1,9 @@
+export const moviesCard = [
+  {
+    title:"DESPATCH",
+    Laguage:"ENGLISH/HINDI",
+    img: "https://akamaividz2.zee5.com/image/upload/w_347,h_521,c_scale,f_webp,q_auto:eco/resources/0-0-1z5642681/portrait/1920x7705f4b8b13cdbf462b9c08f37b9498e87d4ba34f807a7541bf9e8a73f9ee91e59d.jpg",
+  },
+
+
+];
---
+++ index.js
@@ -0,0 +1,8 @@
+// import { moviesCard } from "./data/data.js";
+
+// moviesCard.forEach((movie) => {
+//   console.log(movie.title);
+//   console.log(movie.Laguage);
+//   console.log(movie.img);
+// });
+
